191e689934534cfc2d305b5f2d5b2b90c559a798
qettu <50637084+qettu@users.noreply.github.com>
2025-06-27 10:23:05 +0300

    animations, better divs
    Animated title and circle, changed title and circle centering type

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var test = document.getElementById("test");
 
 var paperielementti = document.getElementById("muistiopaperi1");
-var tekstielementti = document.getElementById('tekstialue');
+var kaikkiTekstielementit = document.getElementsByClassName('tekstialue');
 
-// Fontti säädetään muistilapun rivien mukaan (1.5 rivivälillä täytyy jakaa 33.267 muistilappukuva)
+// Fontti säädetään tekstin koko ensimmäisen muistilapun rivien mukaan 
+// (1.5 rivivälillä täytyy jakaa 33.267 muistilappukuva)
 function adjustFontSize() {
 	const windowWidth = window.innerWidth;
-	tekstielementti.style.fontSize = muistiopaperi1.offsetHeight/33.267+'px';
+
+	for (var i = 0; i < kaikkiTekstielementit.length; i++) {
+		kaikkiTekstielementit[i].style.fontSize = muistiopaperi1.offsetHeight/33.267+'px';
+	}
 }
+
 // Säädetään fontti jo alussa oikein
 adjustFontSize();
 // Säädetään fontti koon muuttuessa
@@ -32,4 +37,7 @@ document.onscroll = function() {
 			document.body.style.background = "background_img no-repeat center center fixed";
 		}				
 	}
+	
+	// Fontti ei aina lataudu, kun muistilappu ei näy alussa. Pitäisi ehkä päivittää rullatessa
+	adjustFontSize();
 };
