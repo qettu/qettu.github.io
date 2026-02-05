566b8e8ac7baac5cddadeb532e2ff68f1de5363f
qettu <task-decoy-humid@duck.com>
2026-02-05 10:42:32 +0200

    Uses only 1 fixed javascript code

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// main
 
-var test = document.getElementById("test"); /*TODO: remove*/
-var paperielementti = document.getElementById("muistiopaperi1");
+var paperielementti = document.getElementsByClassName("muistiopaperi");
 var kaikkiTekstielementit = document.getElementsByClassName('tekstialue');
 
 // Fontti säädetään tekstin koko ensimmäisen muistilapun rivien mukaan 
@@ -10,9 +8,10 @@ function adjustFontSize() {
 	const windowWidth = window.innerWidth;
 
 	for (var i = 0; i < kaikkiTekstielementit.length; i++) {
-		kaikkiTekstielementit[i].style.fontSize = muistiopaperi1.offsetHeight/33.267+'px';
+		kaikkiTekstielementit[i].style.fontSize = paperielementti[0].offsetHeight/33.267+'px';
 	}
 }
+
 
 
 // Säädetään fontti jo alussa oikein
@@ -33,12 +32,12 @@ document.onscroll = function() {
 		{
 			curDiv = allDivs[i];
 			heightBefore = 100;
-			console.log("scrolled from top:")
-			console.log(scrollTop);
-			console.log("Offset from top:")
-			console.log(curDiv.offsetTop);
-			console.log(heightBefore);
-			console.log(curDiv.offsetTop - heightBefore);
+			//console.log("scrolled from top:")
+			//console.log(scrollTop);
+			//console.log("Offset from top:")
+			//console.log(curDiv.offsetTop);
+			//console.log(heightBefore);
+			//console.log(curDiv.offsetTop - heightBefore);
 			
 			if (scrollTop > curDiv.offsetTop - heightBefore){
 				console.log("Bg Image change to...");
@@ -81,13 +80,29 @@ function log(text, tekstialue) {
 tekstialue1 = document.getElementById("tekstialue1");
 tekstialue2 = document.getElementById("tekstialue2");
 tekstialue3 = document.getElementById("tekstialue3");
+tekstialue4 = document.getElementById("tekstialue4");
 
-log("Sopwith F-1 Camel modeled with Blender \n\n I created this model along with its textures for a shortfilm, but the project is still in progress (the background is in the early stage, and the character is in the making). Most of the textures are procedural.", tekstialue1)
-log("Futuristic corridor \n\n A simple 3D model with only procedural textures. ", tekstialue2)
-log("I have coded a program (using C++ and LunarG's Vulkan SDK) that can load 3D models and textures from .gltf files and pre-mipmapped KTX texture files. The base of the program is built by following Alexander Overvoorde's Vulkan tutorial (hyperlink below). I have later added Blinn-Phong lighting and some other features. The main code includes 3,000 lines of C++-code. In addition, the program contains GLSL-coded shaders translated into SPIR-V. ", tekstialue3)
-log("This old project combined wildlife photografing and programming. The Android app, made with Android Studio and Java, allows the user to save butterfly observations (including weather, temperature, location and species) and browse common butterflies based on their characteristics.", tekstialue4)
+if(tekstialue1!=null){
+	console.log("luku 1")
+	log("Sopwith F-1 Camel modeled with Blender \n\n I created this model along with its textures for a shortfilm, but the project is still in progress (the background is in the early stage, and the character is in the making). Most of the textures are procedural.", tekstialue1)
+}
+if(tekstialue2!=null){
+	console.log("luku 2")
 
+	log("Futuristic corridor \n\n A simple 3D model with only procedural textures. ", tekstialue2)
+}
+if(tekstialue3!=null){
+	console.log("luku 3")
 
+	log("I have coded a program (using C++ and LunarG's Vulkan SDK) that can load 3D models and textures from .gltf files and pre-mipmapped KTX texture files. The base of the program is built by following Alexander Overvoorde's Vulkan tutorial (hyperlink below). I have later added Blinn-Phong lighting and some other features. The main code includes 3,000 lines of C++-code. In addition, the program contains GLSL-coded shaders translated into SPIR-V. ", tekstialue3)
+}
+if(tekstialue4!=null){
+	console.log("luku 4")
+
+	log("This old project combined wildlife photografing and programming. The Android app, made with Android Studio and Java, allows the user to save butterfly observations (including weather, temperature, location and species) and browse common butterflies based on their characteristics.", tekstialue4)
+}
+
+// Finnish translations
 //log("Blenderillä mallinnettu Sopwith F-1 Camel.\n\n - Malli on toteutettu alusta asti itse.\n - Kaikki tekstuurit ovat itse valokuvattuja tai proseduraalisesti luotuja.\n - Projekti on edelleen kesken. Seuraava tavoite on maisema ja animoitu hahmo, joilla olisi mahdollista toteuttaa lyhyt, loppuun asti renderöity ja jälkiprosessoitu lyhytanimaatio.", tekstialue1)
 //log("Futuristinen käytävä. \n\n Pieni Blenderillä tehty 3D-malli, jonka kaikki materiaalit ovat proseduraalisesti toteutettuja. ", tekstialue2)
 //log("Olen toteuttanut C++-kielellä LunarG:n julkaisemaa Vulkan SDK:ta käyttäen ohjelman, joka pystyy lataamaan 3D-malleja .obj-tiedostoista ja liittämään niihin tekstuureja. Ohjelman runko on toteutettu seuraamalla Alexander Overvoorden Vulkan tutoriaalia (linkki ohessa). Myöhemmin olen lisännyt ohjelmaan Blinn-Phong valaistusmallin ja muutamia muita ominaisuuksia. Pääohjelmassa on yli 3000 riviä C++-koodia. Lisäksi ohjelmaan kuuluu GLSL-koodia. Projekti jatkuu edelleen.", tekstialue3)
